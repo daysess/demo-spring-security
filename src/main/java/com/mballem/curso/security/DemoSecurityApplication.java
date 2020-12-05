228af69a098b3315b9dcd9e5b10b64cd087bc157
Dayse Soares <daysedesign@gmail.com>
2020-12-04 22:11:44 -0300

    preparando o acesso ao usuario e criando o perfil admin

--- src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
+++ src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoSecurityApplication {
 
 	public static void main(String[] args) {
+		//System.out.println(new BCryptPasswordEncoder().encode("123"));
 		SpringApplication.run(DemoSecurityApplication.class, args);
 	}
 }
--- src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -1,11 +1,19 @@
 package com.mballem.curso.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.mballem.curso.security.service.UsuarioService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	UsuarioService usuarioService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -29,6 +37,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.logoutSuccessUrl("/");
 		
 	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(usuarioService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	
 	
 	
 
