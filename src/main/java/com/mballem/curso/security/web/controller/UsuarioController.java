cb178b9a938fffa66def4aec55f2e8414f0e723c
Dayse Soares <daysedesign@gmail.com>
2020-12-08 16:12:17 -0300

    Desenvolvido a tela de cadastro de usuarios

--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -1,19 +1,71 @@
 package com.mballem.curso.security.web.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.mballem.curso.security.domain.Perfil;
 import com.mballem.curso.security.domain.Usuario;
+import com.mballem.curso.security.service.UsuarioService;
 
 @Controller
 @RequestMapping("u")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
 
 	// abrir cadastro de usuarios (medico/admin/paciente)
 	@GetMapping({ "/novo/cadastro/usuario" })
 	public String cadastroPorAdminParaAdminMedicoPaciente(Usuario usuario) {
 		return "usuario/cadastro";
 	}
+	
+	// lista de usuarios
+	@GetMapping("/lista")
+	public String listarUsuarios() {
+		return "usuario/lista";
+	}
+	
+	// lista de usuarios na datatables
+	@GetMapping("/datatables/server/usuarios")
+	public ResponseEntity<?> listarUsuariosDatatables(HttpServletRequest request) {
+		Map<String, Object> lista = usuarioService.buscarTodos(request);
+		return ResponseEntity.ok(lista);
+	}
+	
+	// salvar cadastro de usuarios por administrador
+	@PostMapping("/cadastro/salvar")
+	public String salvarUsuarios(Usuario usuario, RedirectAttributes attr) {
+		
+		List<Perfil> perfis = usuario.getPerfis();
+		if(perfis.size() > 2 ||
+				perfis.containsAll(Arrays.asList(new Perfil(1L), new Perfil(3L))) ||
+				perfis.containsAll(Arrays.asList(new Perfil(2L), new Perfil(3L))) ) {
+			attr.addFlashAttribute("falha", "Paciente não pode ser Admin e/ou Médico");
+			attr.addFlashAttribute("usuario", usuario);
+		}else {
+			try {
+				usuarioService.salvarUsuario(usuario);
+				attr.addFlashAttribute("sucesso", "Operação realizada com sucesso.");
+			} catch (DataIntegrityViolationException ex) {
+				attr.addFlashAttribute("falha", "Cadastro não realizado, email já existente.");
+			}
+			
+		}
+		return "redirect:/u/novo/cadastro/usuario";
+	}
+
 
 }
---
+++ src/main/java/com/mballem/curso/security/web/conversor/PerfilConverter.java
@@ -0,0 +1,21 @@
+package com.mballem.curso.security.web.conversor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import com.mballem.curso.security.domain.Perfil;
+
+@Component
+public class PerfilConverter implements Converter<String, List<Perfil>>{
+
+	@Override
+	public List<Perfil> convert(String source) {
+		List<Perfil> perfis = new ArrayList<>();
+		perfis.add(new Perfil(Long.parseLong(source)));
+		return perfis;
+	}
+
+}
